4eae01af3dfde353d7b27cb83d90001d2699af65
Khaled Rahnama <krahnama@c4a2c10.42berlin.de>
2026-06-02 12:42:26 +0200

    why_struct using strcpy to copy strings in memory

--- why_struct.c
+++ why_struct.c
@@ -6,12 +6,18 @@
 /*   By: krahnama <krahnama@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 11:14:59 by krahnama          #+#    #+#             */
-/*   Updated: 2026/06/02 11:49:17 by krahnama         ###   ########.fr       */
+/*   Updated: 2026/06/02 12:40:47 by krahnama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/* In this code, we have defined a struct called "employees" 
+ which contains several members such as name, family, address, age, and salary. 
+ We then create two instances of the struct, e1 and e2, and assign values to their members.
+ Finally, we access and print the values of the members for both instances.
+*/
 
 #include<stdio.h>
+#include<string.h>
 
 int main()
 {
@@ -32,20 +38,35 @@ int main()
         int salary;
     };
 
-    //how use struct
+    //how use struct 
+
     struct employees e1 = {
-        "name of employee",
-        "family of employee ",
-        "address of employee ",
+        "sam",
+        "samfamily ",
+        "No 12, Berlin ",
         23,
         2000
     };
 
+    struct employees e2;
+    
+    strcpy(e2.name, "ram");
+    strcpy(e2.family, "ramfamily");
+    strcpy(e2.address, "No 13, London");
+    e2.age = 24;
+    e2.salary = 3000;
+
+      
     //Accessing members : 
     printf("%s \n", e1.name);
     printf("%s \n", e1.family);
-    printf("%s \n", e1.family);
+    printf("%s \n", e1.address);
     printf("%d \n", e1.age);
-    printf("%d \n", e1.salary);
+    printf("%d \n\n\n", e1.salary);
 
+    printf("%s \n", e2.name);
+    printf("%s \n", e2.family);
+    printf("%s \n", e2.address);
+    printf("%d \n", e2.age);
+    printf("%d \n", e2.salary);
 }
